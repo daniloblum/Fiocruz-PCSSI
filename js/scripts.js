a64c541d4dd0dfb486be01cee7d07a6b18cd63cb
Danilo Blum <dnl.blm@gmail.com>
2023-05-10 20:06:46 +0300

    Compartilhar podcast

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,32 @@
 	$(function () {
 		$('[data-toggle="popover"]').popover(options);
 	});
+
+	// Tooltip
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip(options);
+	});
 })();
+
+// Botão de copiar podcast
+var btnCopyLink = document.querySelectorAll('.copy-to-clip');
+
+btnCopyLink.forEach(element => {
+	element.addEventListener('click', () => {
+		let messageCopy = element.getAttribute('data-link');
+
+		navigator.clipboard.writeText(messageCopy);
+
+		tooltipFeedback();
+	});
+
+	function tooltipFeedback() {
+		let feedback = $('[data-toggle="tooltip"]');
+
+		feedback.tooltip('show');
+
+		element.addEventListener('mouseout', () => {
+			feedback.tooltip('hide');
+		});
+	}
+});
